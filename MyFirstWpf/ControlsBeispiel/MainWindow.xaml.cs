5cec69cdf70fa9f6901d7ae96bdfb7448814051b
Mateusz Kowalski <mateusz.kowalski@technikum-wien.at>
2016-11-09 12:01:30 +0100

    DataContext im XAML hinzugefügt

--- MyFirstWpf/ControlsBeispiel/MainWindow.xaml.cs
+++ MyFirstWpf/ControlsBeispiel/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ControlsBeispiel
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
