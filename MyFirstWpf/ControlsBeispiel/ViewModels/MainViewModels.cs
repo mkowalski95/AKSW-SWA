46c5c7c5300a11eaf88c09444b1b8e1fa6f8350e
Mateusz Kowalski <mateusz.kowalski@technikum-wien.at>
2016-11-09 11:56:39 +0100

    MainViewModels hinzugefügt

---
+++ MyFirstWpf/ControlsBeispiel/ViewModels/MainViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlsBeispiel.ViewModels
+{
+    class MainViewModels
+    {
+        private string title;
+        private double price;
+
+        #region Properties
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        #endregion
+    }
+}
